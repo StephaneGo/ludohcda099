01dbe652fd8b42f3e115d55da7585351cc0839fe
Stephane <sgobin@campus-eni.fr>
2025-11-03 16:29:30 +0100

    version avec authentification JWT

---
+++ src/main/java/fr/eni/ludotheque/security/SecurityConfig.java
@@ -0,0 +1,87 @@
+package fr.eni.ludotheque.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Autowired
+	private JwtAuthFilter jwtAuthFilter;
+
+	@Autowired
+	private LudoUserDetailsService userDetailsService;
+
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+		//.csrf(csrf -> csrf.disable())
+		.csrf(AbstractHttpConfigurer::disable)  //Spring security 6 !!
+		// .csrf(withDefaults())
+			.authorizeHttpRequests((requests) -> requests
+					//Accès public au catalogue de jeux
+				.requestMatchers(HttpMethod.GET, "/api/jeux", "/api/*/jeux").permitAll()
+				//.requestMatchers( "/login").permitAll()
+					.requestMatchers(HttpMethod.POST, "/api/v1/login").permitAll()
+				.requestMatchers( "/api/clients", "/api/clients/**").hasRole("EMPLOYE")
+				.anyRequest().authenticated()
+			)
+				.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authenticationProvider(authenticationProvider())
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    //return new BCryptPasswordEncoder();
+		//return NoOpPasswordEncoder.getInstance(); //sans gestion du chiffrement
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailsService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		return authenticationProvider;
+
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+
+	
+//	@Bean
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user =
+//			 User.withDefaultPasswordEncoder()
+//				.username("user")
+//				.password("password")
+//				.roles("USER")
+//				.build();
+//
+//		return new InMemoryUserDetailsManager(user);
+//	}
+}
